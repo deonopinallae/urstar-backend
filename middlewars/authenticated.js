fd1bf078e70abd467137df4b4d03c98769cabf95
deonopinallae <deonopinallae@yandex.ru>
2025-11-24 12:23:34 +0300

    fix deploy

--- middlewars/authenticated.js
+++ middlewars/authenticated.js
@@ -1,15 +1,17 @@
-import { verify } from "../helpers/index.js"
-import { User } from "../models/index.js"
+import { verify } from '../helpers/index.js'
+import { User } from '../models/index.js'
 
-export const authenticated = async(req, res, next) => {
-    const tokenData = verify(req.cookies.token)
-    const user = await User.findOne({_id: tokenData.id}) 
-    if(!user){
-        res.send({error: 'authenticated user not found'})
-        return
-    }
-    req.user = user
-    next()
+export const authenticated = async (req, res, next) => {
+	const token = req.headers.authorization?.split(' ')[1]
+	if (!token) return res.status(401).json({ error: 'JWT must be provided' })
+
+	try {
+		const tokenData = verify(token)
+		const user = await User.findById(tokenData.id)
+		if (!user) return res.status(401).json({ error: 'User not found' })
+		req.user = user
+		next()
+	} catch (err) {
+		return res.status(401).json({ error: 'Invalid token' })
+	}
 }
-
-
