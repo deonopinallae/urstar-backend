1c67eb31759a0fc01f3c5a360a6d39f7a820d51d
deonopinallae <deonopinallae@yandex.ru>
2025-11-27 10:21:50 +0300

    fix

--- app.js
+++ app.js
@@ -20,14 +20,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(
-	cors({
-		origin: 'https://urstar.onrender.com',
-		credentials: true,
-	}),
+  cors({
+    origin: ['https://urstar.vercel.app', 'https://urstar.onrender.com'],
+    credentials: true
+  })
 )
 
 app.use('/api', router)
-// app.use(express.static('../frontend/build'))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
--- routers/auth.js
+++ routers/auth.js
@@ -6,29 +6,45 @@ export const authRouter = express.Router({ mergeParams: true })
 
 authRouter.post('/register', async (req, res) => {
 	try {
-		const { user, token } = await register(req.body.login, req.body.password, req.body.registeredAt)
-		res.cookie('token', token, { httpOnly: true }).send({
+		const { user, token } = await register(
+			req.body.login,
+			req.body.password,
+			req.body.registeredAt,
+		)
+		res.cookie('token', token, {
+			httpOnly: true,
+			sameSite: 'none',
+			secure: true,
+		}).send({
 			error: null,
-			user: mapUser(user)
+			user: mapUser(user),
 		})
 	} catch (e) {
-		res.send({ error: e.message || 'Unknow error' })
+		res.send({ error: e.message || 'Unknown error' })
 	}
 })
 
 authRouter.post('/login', async (req, res) => {
 	try {
 		const { user, token } = await login(req.body.login, req.body.password)
-		res.cookie('token', token, { httpOnly: true }).send({
+		res.cookie('token', token, {
+			httpOnly: true,
+			sameSite: 'none',
+			secure: true,
+		}).send({
 			error: null,
 			user,
-			token
+			token,
 		})
 	} catch (e) {
-		res.send({ error: e.message || 'Unknow error' })
+		res.send({ error: e.message || 'Unknown error' })
 	}
 })
 
 authRouter.post('/logout', (req, res) => {
-	res.cookie('token', '', { httpOnly: true }).send({})
+	res.cookie('token', '', {
+		httpOnly: true,
+		sameSite: 'none',
+		secure: true,
+	}).send({})
 })
